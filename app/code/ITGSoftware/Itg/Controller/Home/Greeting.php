8ea3c996803509348095438d23807a6c93ef9ebf
othmantitee <othman22022@gmail.com>
2019-01-17 15:07:49 +0200

    Edit Greeting contorller

--- app/code/ITGSoftware/Itg/Controller/Home/Greeting.php
+++ app/code/ITGSoftware/Itg/Controller/Home/Greeting.php
@@ -18,23 +18,15 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
         protected $_pageFactory;
 
         /**
-         * @var Magento\Catalog\Api\ProductRepositoryInterface
-         */
-        protected $productRepository;
-
-        /**
          * @param \Magento\Framework\App\Action\Context $context
          * @param \Magento\Framework\View\Result\PageFactory $pageFactory
-         * @param Magento\Catalog\Api\ProductRepositoryInterface $productRepository
          * @return \Magento\Framework\App\Action\Action
          */
         public function __construct(
             \Magento\Framework\App\Action\Context $context,
-            \Magento\Framework\View\Result\PageFactory $pageFactory,
-            ProductRepositoryInterface $productRepository
+            \Magento\Framework\View\Result\PageFactory $pageFactory
             ){
             $this->_pageFactory = $pageFactory;
-            $this->productRepository = $productRepository;
             return parent:: __construct($context);
         }
         
@@ -43,7 +35,6 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
          */
         public function execute()
         {
-            $prodcut = $this->productRepository->getById(1);
             return $this->_pageFactory->create();
         }
     }
